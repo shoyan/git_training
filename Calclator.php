2865bcad4237aaae06d7ed55cdeb11548c3d9821
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2012-08-30 11:22:11 +0900

    掛け算とわり算を実装しました

--- Calclator.php
+++ Calclator.php
@@ -12,6 +12,16 @@ class Calclator
         return $a - $b;
     }
 
+    function multiply($a, $b)
+    {
+        return $a * $b;
+    }
+
+    function divide($a, $b)
+    {
+        return $a / $b;
+    }
+
     function square($a)
     {
         return $a * $a;
